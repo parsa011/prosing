bc9d028d127179b80ba523357e352d3c3796dd13
parsa011 <parsa32569@gmail.com>
2022-01-01 23:29:43 +0330

    colorful log for result of test

--- test/test.c
+++ test/test.c
@@ -1,23 +1,46 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <stdarg.h>
+#include <stdlib.h>
 #include "../prosing.h"
+
+#define DEFAULT "\033[0m"
+#define FAILED "\033[0;31m"
+#define SUCCESS "\033[0;32m"
 
 bool test_string_length()
 {
-	return 5 == prosing_string_lenght("parsa");
+	return 6 == prosing_string_lenght("parsa");
 }
 
-bool (*test_function[])() = {
+const char *test_functions_names[] = {
+	"test string length"
+};
+
+bool (*test_functions[])() = {
 	test_string_length
 };
 
+void log_res(char *state,const char *fmt,...)
+{
+    printf("%s",state);
+	char msg[256];
+	va_list ap;
+	va_start(ap,fmt);
+	vsnprintf(msg,256,fmt,ap);
+	va_end(ap);
+	printf("result : %s%s",DEFAULT,msg);
+	putchar('\n');
+}
+
 int main(int argc,char *argv)
 {
-    printf("Hello ,i'm working :))\n");
-    int test_func_count = sizeof(test_function) / sizeof(test_function[0]);
+    int test_func_count = sizeof(test_functions) / sizeof(test_functions[0]);
     for (int i = 0;i < test_func_count;i++) {
-		if ((*test_function[i])() == true) {
-			printf("TEST DONE\n");
+		if ((*test_functions[i])() == true) {
+    		log_res(SUCCESS,"'%s' passed",test_functions_names[i]);
+		} else {
+    		log_res(FAILED,"'%s' failed",test_functions_names[i]);
 		}
     }
     return 0;
