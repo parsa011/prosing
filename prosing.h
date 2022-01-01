a97ee0fd0e06da7b0f27c6e64623a4336db0c25b
parsa011 <parsa32569@gmail.com>
2022-01-01 22:51:55 +0330

    very simpel test function and string length

--- prosing.h
+++ prosing.h
@@ -11,8 +11,32 @@ typedef struct string_t {
 } string;
 
 /*
- *	functions declerations
+ *	this is main routine for create new string
+ *	we will caluculate value len
  */
-int prosing_string_lenght(string *);
+string *prosing_string_init(char *value);
+
+/*
+ *	reverse string and save it into given string
+ */
+void prosing_string_reverse(string *);
+
+/*
+ *	functions declerations, work with char
+ *  because string struct have len prop and we
+ *  dont need to call function to calculate string
+ *  len
+ */
+int prosing_string_lenght(char *);
+
+
+int prosing_string_lenght(char *str)
+{
+	int res = 0;
+	char *temp = str;
+	while (*temp++)
+    	res++;
+	return res;
+}
 
 #endif
--- test/test.c
+++ test/test.c
@@ -1,6 +1,24 @@
+#include <stdio.h>
+#include <stdbool.h>
 #include "../prosing.h"
+
+bool test_string_length()
+{
+	return 5 == prosing_string_lenght("parsa");
+}
+
+bool (*test_function[])() = {
+	test_string_length
+};
 
 int main(int argc,char *argv)
 {
-	return 0;
+    printf("Hello ,i'm working :))\n");
+    int test_func_count = sizeof(test_function) / sizeof(test_function[0]);
+    for (int i = 0;i < test_func_count;i++) {
+		if ((*test_function[i])() == true) {
+			printf("TEST DONE\n");
+		}
+    }
+    return 0;
 }
